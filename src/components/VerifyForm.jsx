47afba9b6b5557af6c5fa85ace223e1170d981b5
Sstark97 <aitorscinfo@gmail.com>
2021-06-13 09:32:08 +0100

    Add Credit Card/ Secret Code validator

--- src/components/VerifyForm.jsx
+++ src/components/VerifyForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import {Text, Flex, Box, FormLabel, Input, Button} from '@chakra-ui/react';
 import '../assets/styles/icons.css';
 import '../assets/styles/role-form.css';
@@ -8,14 +8,26 @@ import ConfirmModal from '../components/ConfirmModal';
 const VerifyForm = () => {
 
     const [showModal, setModal] = useState(false);
-
-    const handleConfirm = () => {
-        setModal(true);
-    }
+    const [notNumber, setIsNaN] = useState(false);
+    const creditCardRef = useRef();
+    const secureCodeRef = useRef();
 
     const handleCloseInParent = () => {
         setModal(false);
     };
+
+    const handleShowIsNaN = () => {
+        setIsNaN(true);
+        setTimeout(() => (setIsNaN(false)),3000);
+    }
+
+    const handleVerifyAccount = () => {
+        if(isNaN(parseInt(creditCardRef.current.value)) || isNaN(parseInt(secureCodeRef.current.value))){
+            handleShowIsNaN()
+        } else {
+            setModal(true);
+        }
+    }
     
     return (
     <Flex flexDirection="column" marginLeft="16rem" marginTop="-2rem">
@@ -24,18 +36,34 @@ const VerifyForm = () => {
 
         <Box width="40%" my="2em">
             <FormLabel fontWeight="semibold" textColor="gray.600">Número de tarjeta</FormLabel>
-            <Input placeholder="Introduce el número de tu tarjeta" height="60px" border="3px solid"></Input>
+            <Input ref={creditCardRef} placeholder="Introduce el número de tu tarjeta" height="60px" border="3px solid"></Input>
 
             <FormLabel fontWeight="semibold" textColor="gray.600" marginTop="2em">Código secreto</FormLabel>
-            <Input placeholder="Introduce el código secreto" height="60px" border="3px solid"></Input>
+            <Input ref={secureCodeRef} placeholder="Introduce el código secreto" height="60px" border="3px solid"></Input>
 
-            <Button marginTop="2em" colorScheme="green" width="100%" height="60px" onClick={handleConfirm}>Crear Cuenta</Button>
+            <Button marginTop="2em" colorScheme="green" width="100%" height="60px" onClick={handleVerifyAccount}>Crear Cuenta</Button>
             <Flex fontSize="small" textColor="gray.600" justifyContent="center" alignItems="center" marginTop="2rem"> 
                 <GiPadlock></GiPadlock> 
                 <Text marginLeft="1rem">Tu información es segura</Text>
             </Flex>
 
             <ConfirmModal open={showModal} handleCloseInParent={handleCloseInParent}/>
+
+            {notNumber ? 
+                
+                <Text 
+                    textAlign="center" 
+                    marginTop="0.8em" 
+                    border="1px solid red" 
+                    borderRadius="8px" 
+                    padding="5px"
+                    bgColor="#dc3545">
+                        Introduce un número válido
+                </Text> 
+                        : 
+                <></> 
+
+            }
             
         </Box>
 
